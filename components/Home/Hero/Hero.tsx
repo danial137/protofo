add8274017cadbd8187366cab965c6804cacc20a
danial137 <danial79fakhrabadi@gmail.com>
2025-08-24 01:29:58 +0330

    feat: enhance Hero component layout and styling for improved readability

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,23 +1,35 @@
 import { BaseInfo } from '@/Data/data'
 import React from 'react'
+import { FaDownload } from 'react-icons/fa'
+
+
 
 const Hero = () => {
   return (
-      <div className='w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#0f0715] overflow-hidden relative'>
-          <div className='flex justify-center flex-col w-4/5 h-full mx-auto '>
-              <div className='grid grid-cols-1 lg:grid-cols-2 items-center gap-12 '>
-                  {/* content */}
-                  <div>
-                      <h1 className='text-2xl md:text-3xl lg:text-4xl text-gray-300 font-semibold'>I am {BaseInfo.name}
-                          
-                      </h1>
-                      <h1 className='text-bg text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold md:leading-[3.5rem] xl:leading-[4rem] text-white'>{BaseInfo.position }</h1>
-                  </div>
-                  {/* image content */}
-                  <div></div>
-          </div>
-          </div>
+    <div className='w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#0f0715] overflow-hidden relative'>
+      <div className='flex justify-center flex-col w-4/5 h-full mx-auto '>
+        <div className='grid grid-cols-1 lg:grid-cols-2 items-center gap-12 '>
+          {/* content */}
+          <div>
+            <h1 className='text-2xl md:text-3xl lg:text-4xl mb-5 text-gray-300 font-semibold'>I am {BaseInfo.name}
+
+            </h1>
+            <h1 className='text-bg text-3xl  sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold md:leading-[3rem] lg:leading-[3.5rem] xl:leading-[4rem] text-white'>{BaseInfo.position}
+
+            </h1>
+            {/* description */}
+            <p className='text-sm md:text-base text-white text-opacity-60 mt-6'>{BaseInfo.description}</p>
+
+            <button className='md:px-8 md:py-2.5 px-6 py-1.5 text-white font-semibold text-sm md:text-lg transition-all duration-200 rounded-lg mt-8 bg-blue-700 hover:bg-blue-900 flex items-center space-x-2'>
+            <span >Download CV</span>
+            <FaDownload />
+          </button>
+        </div>
+        {/* image content */}
+        <div></div>
+      </div>
     </div>
+    </div >
   )
 }
 
